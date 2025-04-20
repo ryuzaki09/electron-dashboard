b13fa2e5aefb6876d4c426f57442da51af383548
Sin Luong <sin@finervision.com>
2025-04-20 11:16:28 +0100

    fix: check weather every hour.

--- src/components/withWeatherForecast/index.tsx
+++ src/components/withWeatherForecast/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import {mainStore} from '../../store/mainStore'
 import {weatherApi} from '../../api/weatherApi'
+import {today} from '../../helpers/time'
+
+const ONE_HOUR = 1000 * 60 * 60
 
 export function WithWeatherForecast({children}: {children: React.ReactNode}) {
+  const [todayDate, setTodayDate] = React.useState(today)
   const weatherData = mainStore((state) => state.weather)
 
   React.useEffect(
     () => {
+      let timeoutId: NodeJS.Timeout
       async function getWeather() {
         const data = await weatherApi.getForecast()
         if (data) {
@@ -16,6 +21,17 @@ export function WithWeatherForecast({children}: {children: React.ReactNode}) {
 
       if (!weatherData) {
         getWeather()
+      }
+
+      timeoutId = setTimeout(() => {
+        if (today !== todayDate) {
+          setTodayDate(today)
+          getWeather()
+        }
+      }, ONE_HOUR)
+
+      return () => {
+        clearTimeout(timeoutId)
       }
     },
     [weatherData]
