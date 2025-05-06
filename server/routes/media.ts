6d195f35dbff36d010509e3173e3c4a992898e58
Sin Luong <sin@finervision.com>
2025-05-06 11:48:26 +0100

    removed unused code.

--- server/routes/media.ts
+++ server/routes/media.ts
@@ -9,16 +9,6 @@ let musicMediaCache: IFile[] | null = null
 let lastCacheTime = 0
 const CACHE_DURATION = 1000 * 60 * 120 // 120minutes
 
-router.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, DELETE')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
-
 const allowedFileExtensions = ['.mkv', '.mp4', '.avi']
 
 router.get('/videos', (_req: express.Request, res) => {
