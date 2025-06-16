e0d14c92aba6890f843f8d96183cd485b8c05fe1
Sin Luong <sin@finervision.com>
2025-06-16 11:41:07 +0100

    chore: added modal for weather forecast.

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {format} from 'date-fns'
+import classnames from 'classnames'
 
 import {Container} from '../components/container'
 import {useTime} from '../hooks/useTime'
@@ -32,37 +33,82 @@ function TimeDisplay() {
 }
 
 function WeatherInformation() {
+  const [forecastIsOpen, setForecastIsOpen] = React.useState(false)
   const weatherData = mainStore((state) => state.weather)
+  const weatherWrapperRef = React.useRef<HTMLDivElement>(null)
 
   if (!weatherData) {
     return <div>Loading...</div>
   }
 
-  const {weather, temp, daily} = weatherData
+  const {weather, daily} = weatherData
   const WeatherIcon = weather ? weather.icon : null
+  const [todayWeather] = daily
 
   return (
-    <div className={styles.weather}>
-      <div>
-        {WeatherIcon && <WeatherIcon />}
+    <>
+      <div
+        className={styles.weather}
+        ref={weatherWrapperRef}
+        onClick={() => setForecastIsOpen(true)}
+      >
         <div>
-          {weather?.main} {temp}&deg;C
+          {WeatherIcon && <WeatherIcon />}
+          <div>
+            {weather.main} {todayWeather.temp.max}
+            &deg;C
+          </div>
+        </div>
+        <div className={styles.dailyWeather}>
+          {daily.length > 0 &&
+            daily.map((d, index: number) => {
+              const Icon = d.weather.icon
+              return index > 0 && index < 4 ? (
+                <div key={d.dt.day}>
+                  <Icon />
+                  <p>
+                    {d.dt.day}
+                    <br />
+                    {d.temp.min}
+                    &deg;C / {d.temp.max}
+                    &deg;C
+                  </p>
+                </div>
+              ) : null
+            })}
         </div>
       </div>
-      <div className={styles.dailyWeather}>
-        {daily.length > 0 && daily.map((d, index: number) => {
-          const Icon = d.weather.icon
-          return index > 0 && index < 4 ? (
-            <div key={d.dt.day}>
-              <Icon />
-              <p>
-                {d.dt.day}<br />
-                {d.temp.min}&deg;C / {d.temp.max}&deg;C
-              </p>
-            </div>
-          ) : null
+      <div
+        className={classnames(styles.forecastModal, {
+          [styles.modalActive]: forecastIsOpen
         })}
+      >
+        <div>
+          <span
+            onClick={() => {
+              setForecastIsOpen(false)
+            }}
+          >
+            ✕
+          </span>
+        </div>
+        <div className={styles.forecastModalContent}>
+          {daily.length > 0 &&
+            daily.map((d) => {
+              const Icon = d.weather.icon
+              return (
+                <div key={`forecast_${d.dt.day}`}>
+                  <Icon />
+                  {d.dt.day}
+                  <br />
+                  {d.temp.min}
+                  &deg;C / {d.temp.max}
+                  &deg;C
+                </div>
+              )
+            })}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
