0a61450b2d4644de170b9d7298fd18ba53c00cc6
Sin Luong <sin@finervision.com>
2026-01-10 21:54:32 +0000

    style: photo album selection.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classnames from 'classnames'
 import {Link} from 'react-router-dom'
-import {Icon, Checkbox, ModernDropdown} from '@ryusenpai/shared-components'
+import {Icon, ModernDropdown} from '@ryusenpai/shared-components'
 
 import HomeIcon from '../icons/home'
 import MusicIcon from '../icons/music'
@@ -14,7 +14,7 @@ import {CustomModal} from '../modal/modal'
 import {MediaControls} from './mediaControls'
 import {mainStore} from '../../store/mainStore'
 import {useActivityDetection} from '../../hooks/useActivityDetection'
-import type {TImmichAlbumViewDto} from '../../api/types'
+import {TImmichAlbumViewDto} from '../../api/types'
 
 import styles from './index.module.css'
 
@@ -22,15 +22,21 @@ export function Header() {
   const [settingsModalOpen, setSettingsModalOpen] = React.useState(false)
   const {isListening, setIsListening} = useVoiceAssistant()
   const [navIsOpen, setNavIsOpen] = React.useState(false)
-  const [selectedAlbums, setSelectedAlbums] = React.useState<TImmichAlbumViewDto[]>([])
+  const [selectedAlbums, setSelectedAlbums] = React.useState<
+    TImmichAlbumViewDto[]
+  >([])
   const {activateDetection} = useActivityDetection({
     timeout: 5000,
     delayFn: () => setNavIsOpen(false)
   })
   const [showSettings, setShowSettings] = React.useState(false)
-  const {photoAlbums, selectedPhotoAlbums, setSelectedPhotoAlbums, fetchPhotoAlbums, setTheme} = mainStore(
-    (state) => state
-  )
+  const {
+    photoAlbums,
+    selectedPhotoAlbums,
+    setSelectedPhotoAlbums,
+    fetchPhotoAlbums,
+    setTheme
+  } = mainStore((state) => state)
   console.log('isListening: ', isListening)
 
   const onChangeTheme = (theme: {text: string; value: string}) => {
@@ -58,9 +64,12 @@ export function Header() {
     [navIsOpen]
   )
 
-  React.useEffect(() => {
-    setSelectedAlbums(selectedPhotoAlbums)
-  }, [selectedPhotoAlbums])
+  React.useEffect(
+    () => {
+      setSelectedAlbums(selectedPhotoAlbums)
+    },
+    [selectedPhotoAlbums]
+  )
 
   const handleOnClickSettingsMenu = () => {
     if (showSettings) {
@@ -71,12 +80,17 @@ export function Header() {
     setNavIsOpen((prevState) => !prevState)
   }
 
-  const handleOnSelectAlbum = (album: TImmichAlbumViewDto, isChecked: boolean) => {
+  const handleOnSelectAlbum = (
+    album: TImmichAlbumViewDto,
+    isChecked: boolean
+  ) => {
     if (isChecked) {
       return setSelectedAlbums((prevState) => prevState.concat(album))
     }
 
-    return setSelectedAlbums((prevState) => prevState.filter((p) => p !== album))
+    return setSelectedAlbums((prevState) =>
+      prevState.filter((p) => p !== album)
+    )
   }
 
   const onCloseModal = () => {
@@ -142,30 +156,53 @@ export function Header() {
                 />
               </div>
               <div className={styles.modalContent}>
-                <p>Photo Album</p>
-
-                <div className={styles.modalContentPhotoAlbums}>
-                  {photoAlbums.length > 0 &&
-                    photoAlbums.map((a) => {
-                      const found = selectedAlbums.find((s) => s.albumName === a.albumName)
-                      return (
-                        <div key={a.albumName}>
-                          <Checkbox
-                            label={a.albumName}
-                            checked={!!found}
-                            onChangeHandler={(isChecked) =>
-                              handleOnSelectAlbum(a, isChecked)
-                            }
-                          />
-                        </div>
-                      )
-                    })}
-                </div>
+                <PhotoAlbums
+                  albums={photoAlbums}
+                  selectedAlbums={selectedAlbums}
+                  onSelectFn={handleOnSelectAlbum}
+                />
               </div>
             </>
           }
         />
       )}
+    </>
+  )
+}
+
+interface IPhotoAlbumsProps {
+  albums: TImmichAlbumViewDto[]
+  selectedAlbums: TImmichAlbumViewDto[]
+  onSelectFn: (album: TImmichAlbumViewDto, isChecked: boolean) => void
+}
+
+function PhotoAlbums({albums, selectedAlbums, onSelectFn}: IPhotoAlbumsProps) {
+  return (
+    <>
+      <p>Select Photo Album(s)</p>
+
+      <div className={styles.modalContentPhotoAlbums}>
+        {albums.length > 0 &&
+          albums.map((a) => {
+            const found = selectedAlbums.find(
+              (s) => s.albumName === a.albumName
+            )
+            return (
+              <div
+                key={a.albumName}
+                className={styles.albumRow}
+                onClick={() => onSelectFn(a, !found)}
+              >
+                <div>{a.albumName}</div>
+                {found && (
+                  <div>
+                    <div className={styles.check} />
+                  </div>
+                )}
+              </div>
+            )
+          })}
+      </div>
     </>
   )
 }
